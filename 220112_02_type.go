03693465e4416da3b0e3dbb8ec8046d25919b7a7
egeg1212 <dana1anne@naver.com>
2022-01-13 16:19:14 +0900

    :=

---
+++ 220112_02_type.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// const name string = "nico"
+	// name = "Lynn"
+	// fmt.Println(name)
+
+	name := "anne" //👇아래와같다. 축약형은 func안에서만가능하고, 변수var에대해서만 가능하다.
+	// var name string = "anne"
+	name = "jjoo"
+	fmt.Println(name)
+
+}
